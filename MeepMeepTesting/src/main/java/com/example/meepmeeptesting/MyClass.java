db0c61a348e81ee09b6013c90657c4e6f371396b
codesad <sad@stephn.codes>
2024-01-05 23:48:28 +0200

    meep meep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MyClass.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
@@ -8,7 +9,23 @@ import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MyClass {
+    public static double TILE_SIZE = 24.0; // inch
+    public static double ROBOT_HEIGHT_HALF = 9.84; // inch TODO
+
+    public enum TSEPosition {
+        LEFT,
+        CENTER,
+        RIGHT
+    }
+
+    public static double HEADING_TO_BLUE = Math.toRadians(90);
+    public static double HEADING_TO_RED = Math.toRadians(-90);
+    public static double HEADING_TO_BACKDROP = Math.toRadians(0);
+    public static double HEADING_TO_STACK = Math.toRadians(180);
+    static Pose2d startPose = new Pose2d(TILE_SIZE * 0.5 + 3.15, -(-TILE_SIZE * 3 + 18), HEADING_TO_RED);
     public static void main(String[] args) {
+
+
         MeepMeep meepMeep = new MeepMeep(600);
 
         // Declare our first bot
@@ -17,15 +34,14 @@ public class MyClass {
                 .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(10)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(startPose)
+//                                .forward(10)
+//                                .splineTo(new Vector2d(16.5, 30), Math.toRadians(HEADING_TO_BACKDROP))
+//                                .splineToConstantHeading(new Vector2d(49.5,  21.0), Math.toRadians(HEADING_TO_BACKDROP))
+                                .setReversed(true)
+                                .splineTo(new Vector2d(13, 7), Math.toRadians(180))
+                                .splineTo(new Vector2d(0, 8), Math.toRadians(180))
+                                .splineToConstantHeading(new Vector2d(-20, 4.8), Math.toRadians(180))
                                 .build()
                 );
 
